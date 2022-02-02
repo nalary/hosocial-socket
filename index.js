741c43a4f4c6bc5d8cef8d770b95bfa36fe7781f
HO <nalary@hotmail.com>
2022-02-02 14:14:14 +0900

    commit on 1414

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
     cors: {
-        origin: "http://localhost:3000/",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"],
         credentials: true
     }
